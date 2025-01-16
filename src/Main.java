886356cf42c2b8dcabc0422347cf353c6012c214
Eduardo <eduardoherrerabarra@gmail.com>
2025-01-16 12:15:42 -0500

    .

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String []args){
         System.out.println("Hola Mundo!");
-        Sytsem.out.println("Hello")
+        Sytsem.out.println("Hello");
 
     }
 }
